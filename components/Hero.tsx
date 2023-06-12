080d2d95d38aa16b34c19b5027455e6e5f91ba5e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-12 18:12:14 -0400

    Show car and overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,12 @@
 "use client";
 import CustomButton from "./CustomButton";
+import Image from "next/image";
 
 const Hero = () => {
     const handleScroll = () => {};
 
     return (
-        <div className="hero">
+        <div className="hero darK:bg-gray-900">
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero__title">
                     Encuentre, reserve o alquile un automóvil, ¡rápida y
@@ -20,6 +21,18 @@ const Hero = () => {
                     containerStyles="bg-primary-blue text-white rounded-full mt-10"
                     handleClick={handleScroll}
                 />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image
+                        src="/hero.png"
+                        alt="Car hero"
+                        fill
+                        className="object-contain"
+                    />
+
+                    <div className="hero__image-overlay" />
+                </div>
             </div>
         </div>
     );
