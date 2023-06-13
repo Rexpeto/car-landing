3930ab00943a89b8b44a8e697cfcd17351e52963
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-12 20:44:56 -0400

    Update add typeBtn

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,5 @@ export interface CustomButttonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    typeBtn?: "button" | "submit";
 }
