baa14777371430589831eb6da5138f67d49870ab
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-13 17:43:57 -0400

    Footer of page

---
+++ components/Footer.tsx
@@ -0,0 +1,45 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+    const date = new Date().getFullYear();
+
+    return (
+        <footer className="flex flex-col text-black-100 dark:text-white mt-5 border-t border-gray-100 dark:border-gray-500">
+            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+                <div className="flex flex-col justify-start items-start gap-6">
+                    <Image
+                        src="/Logo.svg"
+                        alt="logo"
+                        width={32}
+                        height={32}
+                        className="object-contain"
+                    />
+                    <p className="text-base text-gray-400">
+                        {`Car Landing ${date}`}
+                        <br /> Todos los derechos reservados &copy;
+                    </p>
+                </div>
+                <div className="footer__links">
+                    {footerLinks.map(link => (
+                        <div className="footer__link" key={link.title}>
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map(link => (
+                                <Link
+                                    key={link.title}
+                                    href={link.url}
+                                    className="hover:text-primary-blue transition duration-150"
+                                >
+                                    {link.title}
+                                </Link>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
