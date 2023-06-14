10e11b43cc8d8191b5598614ba831520b7ffe3dd
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-14 15:22:41 -0400

    Import searchBar, CustomFilter and SearchManuFacture

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Header from "./Header";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
+import SearchBar from "./SeachBar";
+import CustomFilter from "./CustomFilter";
+import SearchManuFacturer from "./SearchManuFacturer";
 
-export { Hero, CustomButton, Header, Footer };
+export {
+    Hero,
+    CustomButton,
+    Header,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManuFacturer
+};
