b3c569010c133c8b2804da962326ffd4f1e9af5c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-14 15:24:05 -0400

    Interface searchManuFacturerProps

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,13 @@
-import { MouseEventHandler } from "react";
+import React, { MouseEventHandler } from "react";
 
 export interface CustomButttonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     typeBtn?: "button" | "submit";
+}
+
+export interface SearchManuFacturerProps {
+    manuFacturer: string;
+    setManuFacturer: React.Dispatch<React.SetStateAction<string>>;
 }
