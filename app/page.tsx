4b347bb639521a596105dfc4f0ba3193285619b0
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-16 17:55:18 -0400

    New loyout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, SearchBar, CustomFilter } from "@/components";
+import { Hero, SearchBar, CustomFilter, Header, Footer } from "@/components";
 import GridCars from "@/components/gridCars";
 import clientAxios from "@/config/axios";
 
@@ -18,34 +18,41 @@ export default async function Home() {
         !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
     return (
-        <main className="overflow-hidden">
-            <Hero />
-            <div className="mt-12 padding-x padding-y max-width" id="discover">
-                <div className="home__text-container dark:text-white">
-                    <h1 className="text-3xl font-extrabold">
-                        Catálago de carros
-                    </h1>
-                    <p>Explora los carros que te pueden gustar</p>
-                </div>
-                <div className="home__filters">
-                    <SearchBar />
+        <>
+            <Header />
+            <main className="overflow-hidden">
+                <Hero />
+                <div
+                    className="mt-12 padding-x padding-y max-width"
+                    id="discover"
+                >
+                    <div className="home__text-container dark:text-white">
+                        <h1 className="text-3xl font-extrabold">
+                            Catálago de carros
+                        </h1>
+                        <p>Explora los carros que te pueden gustar</p>
+                    </div>
+                    <div className="home__filters">
+                        <SearchBar />
 
-                    <div className="home__filters-container">
-                        <CustomFilter title="combustible" />
-                        <CustomFilter title="año" />
-                        <CustomFilter title="modelo" />
+                        <div className="home__filters-container">
+                            <CustomFilter title="combustible" />
+                            <CustomFilter title="año" />
+                            <CustomFilter title="modelo" />
+                        </div>
                     </div>
+                    {!isDataEmpty ? (
+                        <GridCars cars={allCars} />
+                    ) : (
+                        <div className="hoem__error-container">
+                            <h2 className="dark:text-white font-semibold text-3xl">
+                                Oops!! No hay resultados
+                            </h2>
+                        </div>
+                    )}
                 </div>
-                {!isDataEmpty ? (
-                    <GridCars cars={allCars} />
-                ) : (
-                    <div className="hoem__error-container">
-                        <h2 className="dark:text-white font-semibold text-3xl">
-                            Oops!! No hay resultados
-                        </h2>
-                    </div>
-                )}
-            </div>
-        </main>
+            </main>
+            <Footer />
+        </>
     );
 }
