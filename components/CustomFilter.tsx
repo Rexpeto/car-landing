8ac0b1fe2755783b3857a9aeb8fb31b2f97df776
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-06-20 16:30:48 -0400

    Filters cars

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,13 +4,28 @@ import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { customFilterProps } from "@/types";
 import { BsArrowDownShort } from "react-icons/bs";
+import { updateSearchParams } from "@/utils/getCars";
 
 const CustomFilter = ({ title, options }: customFilterProps) => {
     const [selected, setSelected] = useState(options[0]);
+    const router = useRouter();
+
+    const handleUpdateParams = e => {
+        const { value } = e;
+        const newPathName = updateSearchParams(title, value);
+
+        router.push(newPathName);
+    };
 
     return (
         <div className="w-fit">
-            <Listbox value={selected} onChange={e => setSelected(e)}>
+            <Listbox
+                value={selected}
+                onChange={e => {
+                    setSelected(e);
+                    handleUpdateParams(e);
+                }}
+            >
                 <div className="relative w-fit z-10">
                     <Listbox.Button className="custom-filter__btn dark:bg-gray-800 dark:text-white dark:border-gray-700">
                         <span className="block truncate">{selected.title}</span>
